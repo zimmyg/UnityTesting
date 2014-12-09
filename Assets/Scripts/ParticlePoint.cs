9ee14c2d68515955f1e10e5c2c595584d8542116
zimmyg <timmy_g2@hotmail.com>
2014-12-09 19:11:06 +1100

    Messing about with materials/shaders and a system for dynamic particle-based terrain

---
+++ Assets/Scripts/ParticlePoint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticlePoint : MonoBehaviour {
+
+	public enum ElementType {
+		EARTH,
+		WATER,
+		LAVA,
+		ICE
+	}
+	
+	public ElementType type;
+	public float particleSize;
+
+	Vector3 position;
+	bool isSupported;
+
+	void Start () {
+		position = transform.position;
+		isSupported = true;
+		switch(type) {
+		case ElementType.EARTH:
+			particleSize = 5.0f;
+			break;
+		default:
+			particleSize = 1.0f;
+			break;
+		}
+	}
+	
+	void Update () {
+	}
+}
